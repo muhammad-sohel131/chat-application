5e7e3f0014a771bd881308b7e901bd339299aab4
Sohele <sohelf131@gmail.com>
2021-08-21 11:12:53 -0700

    lesson 2 source code push

---
+++ controller/userController.js
@@ -0,0 +1,8 @@
+// get users
+function getUsers(req, res, next){
+    res.render('users');
+};
+
+module.exports = {
+    getUsers
+}
---
+++ middlewares/common/errorHandler.js
@@ -0,0 +1,27 @@
+const creatError = require('http-errors');
+
+// 404 not found handler
+function notFoundHandler(req, res, next){
+    next(creatError(404, 'Your requested content was not found'));
+}
+
+// default error handler
+function errorHandler(err, req, res, next){
+    res.locals.error = process.env.NODE_ENV === 'development' ? err : {message: err.message};
+    res.status(err.status || 500);
+
+    if(res.locals.html){
+        // response html
+        res.render('error', {
+            title: 'Error Page'
+        });
+    }else {
+        // response json
+        res.json(res.locals.error);
+    }
+};
+
+module.exports = {
+    notFoundHandler,
+    errorHandler
+}
